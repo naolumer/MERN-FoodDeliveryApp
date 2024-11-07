cc8bb5bc62d059526ba58a3b93eca5294b2df940
naolumer <naolumer3@gmail.com>
2024-11-07 18:40:12 +0300

    Install React and Tailwind.Create pages.

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from '../pages/Home'
+import About from '../pages/About'
+import SignUp from '../pages/signUp'
+import Signin from '../pages/Signin'
+import Dashboard from '../pages/Dashboard'
+import Projects from '../pages/Projects'
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes> 
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/sign-in' element={<Signin/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/projects' element={<Projects/>}/>
+    </Routes>
+    
+    
+    </BrowserRouter>
+    
+  )
+}
+
+export default App
