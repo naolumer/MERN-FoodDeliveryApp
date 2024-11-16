c24bc07b93f53f183f709fce9946d5703407c187
naolumer <naolumer3@gmail.com>
2024-11-16 22:35:46 +0300

    cart page functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Cart from './pages/cart/Cart'
 import Footer from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup'
 
 function App() {
+  const [showLogin,setShowLogin] = useState(false)
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
-      <Navbar/>
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="cart" element={<Cart/>}/>
+        <Route path="/cart" element={<Cart/>}/>
         <Route path="/order" element={<PlaceOrder/>}/>
       </Routes>
     </div>
