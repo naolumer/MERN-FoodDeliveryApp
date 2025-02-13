8d03de2d4b72ea034017b0ac1ee3d68d6fa8d0f9
naolumer <naolumer3@gmail.com>
2025-02-13 09:20:30 +0300

    add jwt and stripe secret keys

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,17 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
-
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 
 // app config
 const app = express()
-const port = 3000
+const port = 4000
 
 // middleware
-
 app.use(express.json())
 app.use(cors())
 
@@ -20,13 +22,18 @@ connectDB();
 // api endpoints
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
+
+
 
 app.get("/",(req,res)=>{
-    res.send('API Working')
+    res.send("API Working")
 })
 
 app.listen(port,()=>{
-    console.log(`Server running on http://localhost:${port}`)
+    console.log(`Server Started on http://localhost:${port}`)
 })
 
 // mongodb+srv://naolumer3:<034903Na>@cluster0.5gmud.mongodb.net/?
