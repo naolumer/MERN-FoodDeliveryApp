bb84fc779b579710820c8a0f043106ab44696959
naolumer <naolumer3@gmail.com>
2025-03-20 11:39:26 +0300

    minor changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+const url = process.env.MONGO_URI
+
 export const connectDB = async()=> {
-    await mongoose.connect('mongodb+srv://naolumer3:034903Na@cluster0.5gmud.mongodb.net/food-del').then(()=>console.log("DB Connected"))
+    await mongoose.connect(`${url}/food-del`).then(()=>console.log("DB Connected"))
 }
